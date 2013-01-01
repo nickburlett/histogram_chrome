0157f64345006777b562c39ca9125d15ef33e4bb
Nicholas Burlett <nickburlett@mac.com>
2013-01-01 15:32:28 -0800

    add support for image as baseURI, also make revert faster
    If document.baseURI is an image, handle that properlly.
    Also make the revert faster

--- histogram_chrome/background.js
+++ histogram_chrome/background.js
@@ -1,34 +1,34 @@
+function handleMessage( message, port ) {
+    console.log("Received "+ message.result + " from " + port.portId_ + ", " + port.name);
+    if (message.result == "compute") {
+        var img = $('<img>'),
+            hist = {};
+        img.load( function() {
+            try {
+                img.pixastic('histogram', { average : true, paint:false,color:"rgba(255,255,255,0.8)",returnValue:hist });
+            } catch (e) {
+                chrome.tabs.sendMessage(tab.id, {kind: "securityFailure"});
+            }
+            var cv = img.pixastic('hsl', {lightness:-50}).pixastic('overlayHistogram', {histData:hist, color:"rgba(255,255,255,0.8)"});
+            port.postMessage({kind: "replaceImage", clickSrc: message.clickSrc, data: cv[0].toDataURL()});
+        });
+        img.attr('src', message.clickSrc);
+    } else if (message.result == "revert") {
+        port.postMessage({kind: "revert", clickSrc:message.origSrc});
+    }
+}
+toggleHistogram = function( port ) {
+    port.onMessage.addListener( handleMessage );
+}
+chrome.extension.onConnect.addListener( toggleHistogram );
+
 /**
  * Returns a handler which will open a new window when activated.
  */
 function getAddHistogramHandler() {
     return function(info, tab) {
-        var toggleHistogram = function( port ) {
-            port.onMessage.addListener( function( message ) {
-                if (message.result == "compute") {
-                    var img = $('<img>'),
-                        hist = {};
-                    img.load( function() {
-                        try {
-                            img.pixastic('histogram', { average : true, paint:false,color:"rgba(255,255,255,0.8)",returnValue:hist });
-                        } catch (e) {
-                            chrome.tabs.sendMessage(tab.id, {kind: "securityFailure"});
-                        }
-                        var cv = img.pixastic('hsl', {lightness:-50}).pixastic('overlayHistogram', {histData:hist, color:"rgba(255,255,255,0.8)"});
-                        if (tab.url == info.srcUrl) {
-                            port.postMessage({kind: "navigate", clickSrc: info.srcUrl, data: cv[0].toDataURL()});
-                        } else {
-                            port.postMessage({kind: "replaceImage", clickSrc: info.srcUrl, data: cv[0].toDataURL()});
-                        }
-                    });
-                    img.attr('src', info.srcUrl);
-                } else if (message.result == "revert") {
-                    port.postMessage({kind: "revert", clickSrc:message.origSrc});
-                }
-            });
-        }
-        chrome.extension.onConnect.addListener( toggleHistogram );
-        chrome.tabs.sendRequest(tab.id, {kind: "info", clickSrc: info.srcUrl });
+        console.log("Sending info request to tab "+ tab.id + " with clickSrc: " + info.srcUrl);
+        chrome.tabs.sendRequest(tab.id, { kind: "info", clickSrc: info.srcUrl });
     };
 };
 
--- histogram_chrome/dohistogram.js
+++ histogram_chrome/dohistogram.js
@@ -3,7 +3,8 @@
 //console.log( port )
 // Record the last element to be right-clicked
 $( function() {
-    var clickedEl = null;
+    var clickedEl = null,
+        port = null;
 
     $("body").bind("contextmenu", function(e) {
         clickedEl = e.srcElement;
@@ -12,8 +13,9 @@ $( function() {
     });
 
     function origsrc(elm) {
-        if ( elm != null && elm.hasOwnProperty('histogram_origsrc') && elm.histogram_origsrc !== undefined ) {
-            return elm.histogram_origsrc;
+        o = $(elm).attr('histogram_origsrc')
+        if ( o !== undefined) {
+            return o;
         } else {
             return null
         };
@@ -21,29 +23,37 @@ $( function() {
 
     chrome.extension.onRequest.addListener(function(request, sender) {
         if (request.kind == "info" && clickedEl && clickedEl.hasOwnProperty('src') && clickedEl.src == request.clickSrc) {
-            var port = chrome.extension.connect( sender.id, { name: clickedEl.src } )
+            console.log("received " + request.kind + " request. clickSrc: " + request.clickSrc)
+            if (port === null) {
+                port = chrome.extension.connect( sender.id, { name: clickedEl.src } )
 
-            port.onMessage.addListener(function(request) {
-                if (request.kind == "replaceImage") {
-                    if ( origsrc(clickedEl) == null && clickedEl && clickedEl.src == request.clickSrc) {
-                        clickedEl.histogram_origsrc = clickedEl.src;
-                        clickedEl.src = request.data;
-                        clickedEl = null;
+                port.onMessage.addListener(function(request) {
+                    if (request.kind == "replaceImage") {
+                        if ( origsrc(clickedEl) == null && clickedEl && clickedEl.src == request.clickSrc) {
+                            if (request.clickSrc == document.baseURI) {
+                                var replace = $('<img>').attr('histogram_origsrc', $(clickedEl).attr('src')).attr('src', request.data) ;
+                                $(clickedEl).replaceWith( replace );
+                            } else {
+                                $(clickedEl).attr('histogram_origsrc', request.clickSrc).prop('src', request.data);
+                            }
+                            clickedEl = null;
+                        }
+                    } else if (request.kind == "revert") {
+                        if (origsrc(clickedEl) == request.clickSrc) {
+                            $(clickedEl).prop('src', origsrc(clickedEl)).attr('histogram_origsrc', null);
+                            clickedEl = null;
+                        }
+                    } else if (request.kind =="securityFailure") {
+                        alert("Sorry, the security policy does not allow the histogram to be computed.");
                     }
-                } else if (request.kind == "revert") {
-                    if (origsrc(clickedEl) == request.clickSrc) {
-                        clickedEl.src = clickedEl.histogram_origsrc;
-                        clickedEl.histogram_origsrc = null;
-                        clickedEl = null;
-                    }
-                } else if (request.kind =="securityFailure") {
-                    alert("Sorry, the security policy does not allow the histogram to be computed.");
-                }
-            });
+                });
+            }
             if (origsrc(clickedEl)) {
-                port.postMessage({result:"revert", origSrc:origsrc(clickedEl)});
+                console.log("Posting 'revert' on "+port);
+                port.postMessage({result:"revert", origSrc:origsrc(clickedEl), clickSrc:request.clickSrc});
             } else {
-                port.postMessage({result:"compute"});
+                console.log("Posting 'compute' on "+port);
+                port.postMessage({result:"compute", clickSrc: request.clickSrc});
             }
         }
     });
