045c0f4fe5f5a85c8705654b11a87ac65a67a939
Nicholas Burlett <nickburlett@mac.com>
2013-07-30 20:14:21 -0700

    500px update
    500px.com updated their website, breaking image_histogram. This brings image histogram back to 500px.
    
    Also bump the version number, and update the manifest line wrapping.

--- histogram_chrome/500px.js
+++ histogram_chrome/500px.js
@@ -1,19 +1,46 @@
+(function ($) {
+
+/**
+* @function
+* @property {object} jQuery plugin which runs handler function once specified element is inserted into the DOM
+* @param {function} handler A function to execute at the time when the element is inserted
+* @param {bool} shouldRunHandlerOnce Optional: if true, handler is unbound after its first invocation
+* @example $(selector).waitUntilExists(function);
+*/
+
+$.fn.waitUntilExists	= function (handler, shouldRunHandlerOnce, isChild) {
+	var found	= 'found';
+	var $this	= $(this.selector);
+	var $elements	= $this.not(function () { return $(this).data(found); }).each(handler).data(found, true);
+	
+	if (!isChild)
+	{
+		(window.waitUntilExists_Intervals = window.waitUntilExists_Intervals || {})[this.selector] =
+			window.setInterval(function () { $this.waitUntilExists(handler, shouldRunHandlerOnce, true); }, 500)
+		;
+	}
+	else if (shouldRunHandlerOnce && $elements.length)
+	{
+		window.clearInterval(window.waitUntilExists_Intervals[this.selector]);
+	}
+	
+	return $this;
+}
+
+}(jQuery));
 
 $(function() {
+    $('.copyright_tooltip').waitUntilExists( function() {
         var $clickit = $('<div style="margin-top: 1em;"></div>'),
-            $toggle = $('<a href="#">Toggle Histogram</a>');
+            $toggle = $('<style type="text/css">a.togglehistogram { color:white; } a.togglehistogram:hover { color: grey; }</style><a class="togglehistogram" href="#">Toggle Histogram</a>');
         $toggle.click( function() { 
-            clickedEl = $('#thephoto img')[0];
+            clickedEl = $('.photo_wrap img')[0];
             window.handleRequest( {kind:'info', clickSrc: clickedEl.src} );
         });
         $clickit.append($toggle);
 
-        $('#copyright').append($clickit);
+        $(this).append($clickit);
 
-        $('#thephoto,#lightbox').bind("contextmenu", function(e) {
-            $('#copyright').css('left', e.pageX).css('top', e.pageY).css('position', 'absolute').css('z-index', 50000);
-            $('#copyright').stop(true, true).fadeIn().delay(4000).fadeOut('slow');
-            e.stopPropagation();
-            e.preventDefault();
-        });
+    });
 });
+
